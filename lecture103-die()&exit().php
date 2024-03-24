e901f3fd8444452b4ae3161e901f738484fe6910
MuneebHassanSiddiqui <mmuneebhassan@gmail.com>
2024-03-24 20:00:34 +0500

    PHPmaster

--- lecture103-die()&exit().php
+++ lecture103-die()&exit().php
@@ -1,5 +1,5 @@
 <?php
-
+echo "<h1>DIE() & EXIT() FUNCTION</h1>";
 
 echo '1 >PHP code is embedded directly into HTML documents, allowing for the creation of dynamic web pages.<br>';
 echo '2 >PHP stands for "Hypertext Preprocessor" and was originally created by Danish-Canadian programmer Rasmus Lerdorf.<br>';
