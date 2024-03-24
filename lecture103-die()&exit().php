d2f0148629288a6aaa97b644091a1bcc1d75efad
MuneebHassanSiddiqui <mmuneebhassan@gmail.com>
2024-03-24 18:28:37 +0500

    PHPMaster

---
+++ lecture103-die()&exit().php
@@ -0,0 +1,17 @@
+<?php
+
+
+echo '1 >PHP code is embedded directly into HTML documents, allowing for the creation of dynamic web pages.<br>';
+echo '2 >PHP stands for "Hypertext Preprocessor" and was originally created by Danish-Canadian programmer Rasmus Lerdorf.<br>';
+echo '3 >Many popular content management systems, such as WordPress and Drupal, are built using PHP.<br>';
+die('CODE STOP!!');
+echo '4 >Learning PHP can open up opportunities for building dynamic and interactive websites.<br>';
+echo '5 >PHP is a popular server-side scripting language used for web development.<br>';
+
+
+// echo '1 >PHP code is embedded directly into HTML documents, allowing for the creation of dynamic web pages.<br>';
+// echo '2 >PHP stands for "Hypertext Preprocessor" and was originally created by Danish-Canadian programmer Rasmus Lerdorf.<br>';
+// echo '3 >Many popular content management systems, such as WordPress and Drupal, are built using PHP.<br>';
+// exit('code stop!!');
+// echo '4 >Learning PHP can open up opportunities for building dynamic and interactive websites.<br>';
+echo '5 >PHP is a popular server-side scripting language used for web development.<br>';
